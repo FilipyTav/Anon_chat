008b00ea1170ba93b4407946ebb937e66c2024b7
BiAniTy <drayttc@gmail.com>
2023-01-04 11:14:04 -0300

    Setup client and server

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactElement } from "react";
+import "./styles/App.scss";
+
+interface Props {}
+
+const App: FC<Props> = (): ReactElement => {
+    return <div>Setup</div>;
+};
+
+export default App;
