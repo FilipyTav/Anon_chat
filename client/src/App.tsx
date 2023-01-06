a0737e42e1116c029f9eb418bc5504eab44a519a
BiAniTy <drayttc@gmail.com>
2023-01-06 17:58:31 -0300

    Adds styling to side menu on smaller screens - client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -517,15 +517,21 @@ interface Props { }
 
 const App: FC<Props> = (): ReactElement => {
     const [current_board, set_current_board] = useState<BoardType | null>(null);
+    const [mq, set_mq] = useState<boolean>(
+        window.matchMedia("(min-width: 40em)").matches,
+    );
 
     useEffect(() => {
-        // See if loads a board
         set_current_board(null);
+
+        window
+            .matchMedia("(min-width: 40em)")
+            .addEventListener("change", (e) => set_mq(e.matches));
     }, []);
 
     return (
         <>
-            <Navbar />
+            <Navbar window_matches={mq} />
 
             {current_board ? (
                 <Board data={current_board} />
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,23 @@
 import { FC, ReactElement } from "react";
 
-interface Props {}
+interface Props {
+    window_matches: boolean;
+}
 
-const Navbar: FC<Props> = (): ReactElement => {
+const Navbar: FC<Props> = ({ window_matches }): ReactElement => {
     return (
         <nav>
             <div className="logo">Logo</div>
+
+            {window_matches ? (
+                <></>
+            ) : (
+                <div className="menu">
+                    <span>|</span>
+                    <span>|</span>
+                    <span>|</span>
+                </div>
+            )}
 
             <ul className="links">
                 <li>
