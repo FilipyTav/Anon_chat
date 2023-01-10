ac884c1d014d4e52e2d38e9e84acbac3889063ea
BiAniTy <drayttc@gmail.com>
2023-01-10 16:28:18 -0300

    Finishes signup form validation and confirmation - server

--- server/src/mvc/controllers/users.ts
+++ server/src/mvc/controllers/users.ts
@@ -8,7 +8,7 @@ import {
 } from "express-validator";
 import mongoose, { CallbackError } from "mongoose";
 
-import Board from "../models/Board";
+import User from "../models/User";
 
 const index = async (
     req: Request,
@@ -18,14 +18,54 @@ const index = async (
     res.redirect("/boards");
 };
 
-const create_post = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-): Promise<void> => {
-    const { username, password, confirm_password } = req.body;
+const create_post = [
+    // Validate and sanitize fieldsj
+    body("username", "Username must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("password", "Password must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("confirm_password")
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error(
+                    "Password confirmation does not match password"
+                );
+            }
 
-    res.json({ status: "testing" });
-};
+            // Indicates the success of this synchronous custom validator
+            return true;
+        })
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
 
+    // Process request
+    async (req: Request, res: Response, next: NextFunction) => {
+        const errors: Result<ValidationError> = validationResult(req);
+
+        const { username, password, confirm_password } = req.body;
+
+        // TODO: hash passwords
+
+        // Create new user obj with the validated and sanitized values
+        const user = new User({ username, password });
+
+        if (!errors.isEmpty())
+            return res.status(400).json({ user, errors: errors.array() });
+
+        try {
+            // Data is valid, save user
+            const result = await user.save();
+
+            // Successful: redirect to new category record.
+            res.redirect("/boards");
+        } catch (err) {
+            return next(err);
+        }
+    },
+];
 export { index, create_post };
